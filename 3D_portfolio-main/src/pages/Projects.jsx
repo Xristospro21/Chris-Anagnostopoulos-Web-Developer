5772471f69c329342a07c24be73cbfe2800f8349
Xristospro21 <thedragon2218@gmail.com>
2025-04-24 22:53:32 +0300

    first commit

---
+++ 3D_portfolio-main/src/pages/Projects.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+import { CTA } from "../components";
+import { projects } from "../constants";
+import { arrow } from "../assets/icons";
+
+const Projects = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        My{" "}
+        <span className='blue-gradient_text drop-shadow font-semibold'>
+          Projects
+        </span>
+      </h1>
+
+      <p className='text-slate-500 mt-2 leading-relaxed'>
+      I've worked on a bunch of different projects while learning and growing as a developer. These are some of my favorites—they really helped me build up my skills and try out new ideas. A lot of them are open-source, so if anything catches your eye, you're more than welcome to check out the code or even jump in and contribute. I'd love to hear your ideas and see what we can build together!
+      </p>
+
+      <div className='flex flex-wrap my-20 gap-16'>
+        {projects.map((project) => (
+          <div className='lg:w-[400px] w-full' key={project.name}>
+            <div className='block-container w-12 h-12'>
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={project.iconUrl}
+                  alt='threads'
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+
+            <div className='mt-5 flex flex-col'>
+              <h4 className='text-2xl font-poppins font-semibold'>
+                {project.name}
+              </h4>
+              <p className='mt-2 text-slate-500'>{project.description}</p>
+              <div className='mt-5 flex items-center gap-2 font-poppins'>
+                <Link
+                  to={project.link}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='font-semibold text-blue-600'
+                >
+                  Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt='arrow'
+                  className='w-4 h-4 object-contain'
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default Projects;
